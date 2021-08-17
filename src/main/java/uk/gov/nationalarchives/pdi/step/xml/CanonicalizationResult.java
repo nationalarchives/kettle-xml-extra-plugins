61a6341e420a9679e4ee00d623e16188896a845e
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2021-08-17 10:46:15 +0100

    Adding transform plugin to create W3C Canonical XML

---
+++ src/main/java/uk/gov/nationalarchives/pdi/step/xml/CanonicalizationResult.java
@@ -0,0 +1,42 @@
+package uk.gov.nationalarchives.pdi.step.xml;
+
+public class CanonicalizationResult {
+
+    private final String canonicalXml;
+    private String errorMessage = "";
+    private Boolean hasErrors = false;
+    private Long errorCount = 0L;
+
+    public CanonicalizationResult(final String canonicalXml) {
+        this.canonicalXml = canonicalXml;
+    };
+
+    public String getCanonicalXml() {
+        return canonicalXml;
+    }
+
+    public Boolean hasErrors() {
+        return hasErrors;
+    }
+
+    public void setHasErrors(final Boolean hasErrors) {
+        this.hasErrors = hasErrors;
+    }
+
+    public Long getErrorCount() {
+        return errorCount;
+    }
+
+    public void incrementErrorCount() {
+        errorCount++;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+}
